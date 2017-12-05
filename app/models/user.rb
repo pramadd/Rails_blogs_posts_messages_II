d98d6fb45c2b3308a52d1eca5a5a71a80057d295
Pranathi <neethu@codingdojo.com>
2017-12-05 01:45:16 -0800

    has many through association

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+    EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+    has_many :owners
+    has_many :messages
+    has_many :posts
+
+    
+    has_many :blogs, through: :owners # all the blogs owned by a specific user
+
+    has_many :blog_posts, through: :posts, source: :blog # all the blogs a user has posted on
+
+    validates :email_address, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+    validates :first_name, :last_name, :email_address, presence: true
+end
